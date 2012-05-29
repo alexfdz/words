4a9df7a773de355dc7ec2849b74028fb09021360
Alex Fernandez <alexfdz@gmail.com>
2012-05-29 21:43:34 +0100

    Switch the visibility of the word/translation textview

---
+++ words-widget/src/main/java/com/a2devel/words/service/SwitchVisibilityService.java
@@ -0,0 +1,60 @@
+package com.a2devel.words.service;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+import android.view.View;
+import android.widget.RemoteViews;
+
+import com.a2devel.words.R;
+import com.a2devel.words.widget.WordsWidget;
+
+public class SwitchVisibilityService extends Service {
+	
+	private static final String TAG = "SwitchVisibilityService";
+	public static final String WORD_VISIBLE_KEY = "com.a2devel.words.word_visible";
+	
+    @Override
+    public void onStart(Intent intent, int startId) {
+    	Log.d(TAG, "Started service");
+        boolean isWordVisible = intent.getExtras().getBoolean(WORD_VISIBLE_KEY);
+        RemoteViews view = updateView(this, isWordVisible);
+        
+        ComponentName widget = new ComponentName(this, WordsWidget.class);
+        AppWidgetManager.getInstance(this).updateAppWidget(widget, view);
+    }
+
+    /**
+     * @param context
+     * @return
+     */
+    public RemoteViews updateView(Context context, boolean isWordVisible) {
+    	Log.d(TAG, "isWordVisible = "+isWordVisible);
+        RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.widget_word);
+        if(isWordVisible){
+        	view.setViewVisibility(R.id.word, View.INVISIBLE);
+            view.setViewVisibility(R.id.translation, View.VISIBLE);
+        }else{
+        	view.setViewVisibility(R.id.word, View.VISIBLE);
+            view.setViewVisibility(R.id.translation, View.INVISIBLE);
+        }
+        
+        Intent switchIntent = new Intent(context, SwitchVisibilityService.class);
+        switchIntent.putExtra(SwitchVisibilityService.WORD_VISIBLE_KEY, !isWordVisible);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, switchIntent,
+        	      PendingIntent.FLAG_UPDATE_CURRENT);
+        view.setOnClickPendingIntent(R.id.widget, pendingIntent);
+        
+        return view;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+}
