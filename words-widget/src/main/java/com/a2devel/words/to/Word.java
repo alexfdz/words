0f27721490746ae737c5b95b5586964bdca50922
Alex Fernandez <alexfdz@gmail.com>
2012-05-29 21:44:01 +0100

    Word Transfer object

---
+++ words-widget/src/main/java/com/a2devel/words/to/Word.java
@@ -0,0 +1,20 @@
+package com.a2devel.words.to;
+
+public class Word {
+
+	private String word;
+	private String translation;
+	
+	public String getWord() {
+		return word;
+	}
+	public void setWord(String word) {
+		this.word = word;
+	}
+	public String getTranslation() {
+		return translation;
+	}
+	public void setTranslation(String translation) {
+		this.translation = translation;
+	}
+}
