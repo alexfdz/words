3706e823e634592fde8c41555927659fb202f640
Alex Fernandez <alexfdz@gmail.com>
2012-05-30 18:04:18 +0100

    Update button

--- words-widget/src/main/java/com/a2devel/words/widget/WordsWidget.java
+++ words-widget/src/main/java/com/a2devel/words/widget/WordsWidget.java
@@ -1,13 +1,26 @@
 package com.a2devel.words.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.RemoteViews;
 
+import com.a2devel.words.R;
 import com.a2devel.words.service.UpdateService;
 
 public class WordsWidget extends AppWidgetProvider {
+	
+	@Override
+    public void onEnabled(Context context) {
+    	RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.widget_word);
+    	
+    	Intent updateServiceIntent = new Intent(context, UpdateService.class);
+        PendingIntent pendingIntent = PendingIntent.getService(context, 0, updateServiceIntent,
+        	      PendingIntent.FLAG_UPDATE_CURRENT);
+    	view.setOnClickPendingIntent(R.id.updateButton, pendingIntent);
+    }
 	
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
