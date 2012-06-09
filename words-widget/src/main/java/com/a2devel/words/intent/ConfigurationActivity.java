87b5363c11b1ff2f9b2c0334e5b00694e17902e1
Alex Fernandez <alexfdz@gmail.com>
2012-06-09 18:45:16 +0100

    UpdateIntent instances control

--- words-widget/src/main/java/com/a2devel/words/intent/ConfigurationActivity.java
+++ words-widget/src/main/java/com/a2devel/words/intent/ConfigurationActivity.java
@@ -1,6 +1,9 @@
 package com.a2devel.words.intent;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
@@ -9,7 +12,6 @@ import android.util.Log;
 import android.view.View;
 
 import com.a2devel.words.R;
-import com.a2devel.words.service.UpdateService;
 import com.a2devel.words.widget.WordsWidget;
 
 public class ConfigurationActivity extends PreferenceActivity {
@@ -20,6 +22,11 @@ public class ConfigurationActivity extends PreferenceActivity {
 	protected static final String TAG = "ConfigurationActivity";
     private static final String PREFS_NAME = WordsWidget.class.getSimpleName();
     private int widgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
+    
+    
+    
+    
+    
 
     public ConfigurationActivity() {
         super();
@@ -55,10 +62,16 @@ public class ConfigurationActivity extends PreferenceActivity {
     
     @Override
     public void onBackPressed() {
-        Intent updateIntent = new Intent(this, UpdateService.class);
-		WordsWidget.addIntentData(updateIntent, widgetId);
-        this.startService(updateIntent);
-        setResult(RESULT_OK, updateIntent);
+    	Intent active = new Intent(this, WordsWidget.class);
+        active.setAction(WordsWidget.ACTION_WIDGET_REFRESH);
+        WordsWidget.addIntentData(active, widgetId);
+        PendingIntent actionPendingIntent = PendingIntent.getBroadcast(this, 0, active, 0);
+        AlarmManager alarmManager = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+(10*1000),
+    			(long)10000,  actionPendingIntent);
+    	Log.d(TAG, "Alarm refreshed:" +widgetId);
+        this.startService(active);
+        setResult(RESULT_OK, active);
         finish();
     }
     
--- words-widget/src/main/java/com/a2devel/words/widget/WordsWidget.java
+++ words-widget/src/main/java/com/a2devel/words/widget/WordsWidget.java
@@ -1,5 +1,7 @@
 package com.a2devel.words.widget;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
@@ -12,19 +14,20 @@ import com.a2devel.words.service.UpdateService;
 public class WordsWidget extends AppWidgetProvider {
 	
 	private static final String TAG = "WordsWidget";
-	
 	public static String ACTION_WIDGET_REFRESH = "ActionReceiverRefresh";
+	public static String ACTION_WIDGET_ALARM_REFRESH = "ActionReceiverAlarmRefresh";
 	public static String URI_SCHEME = WordsWidget.class.getName();
+	
+	private Intent updateIntent;
 	
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
-            int[] appWidgetIds) {
-    	
-    	for (int appWidgetId : appWidgetIds) {
-    		Log.d(TAG, "onUpdate appWidgetId:" +appWidgetId);
-    		Intent updateIntent = new Intent(context, UpdateService.class);
-    		WordsWidget.addIntentData(updateIntent, appWidgetId);
-            context.startService(updateIntent);
+            int[] widgetIds) {
+    	for (int widgetId : widgetIds) {
+    		Log.d(TAG, "onUpdate appWidgetId:" + widgetId);
+    		Intent updateIntent = getUpdateIntent(context);
+            addIntentData(updateIntent, widgetId);
+			context.startService(updateIntent);
 		}
     }
     
@@ -33,15 +36,27 @@ public class WordsWidget extends AppWidgetProvider {
     	Log.d(TAG, "onReceive action: " +intent.getAction() + " widgetId: " +intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 
 				AppWidgetManager.INVALID_APPWIDGET_ID));
         if (intent.getAction().equals(ACTION_WIDGET_REFRESH)) {
-        	int appWidgetId =intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 
+        	int widgetId =intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, 
 					AppWidgetManager.INVALID_APPWIDGET_ID);
-            Intent updateIntent = new Intent(context, UpdateService.class);
-            WordsWidget.addIntentData(updateIntent, appWidgetId);
+        	
+        	Intent updateIntent = getUpdateIntent(context);
+            addIntentData(updateIntent, widgetId);
             context.startService(updateIntent);
-        } else {
+            
+        } else if (intent.getAction().equals(ACTION_WIDGET_ALARM_REFRESH)) {
+        	
+            
+        }else {
             super.onReceive(context, intent);
         }
     }
+    
+    public Intent getUpdateIntent(Context context) {
+    	if(updateIntent == null){
+    		updateIntent = new Intent(context, UpdateService.class);
+    	}
+		return updateIntent;
+	}
     
     /**
      * @param intent
@@ -53,4 +68,30 @@ public class WordsWidget extends AppWidgetProvider {
     	intent.setData(data);
     	intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId);
     }
+    
+	/**
+	 * Each time an instance is removed, we cancel the associated AlarmManager.
+	 */
+	@Override
+	public void onDeleted(Context context, int[] widgetIds) {
+		super.onDeleted(context, widgetIds);
+		for (int widgetId : widgetIds) {
+			cancelAlarm(context, widgetId);
+		}
+	}
+
+	/**
+	 * @param context
+	 * @param appWidgetId
+	 */
+	protected void cancelAlarm(Context context, int widgetId) {
+		Intent intent = getUpdateIntent(context);
+		AlarmManager alarmManager = (AlarmManager) context
+				.getSystemService(Context.ALARM_SERVICE);
+		PendingIntent pendingIntentAlarm = PendingIntent.getBroadcast(context,
+				0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+		alarmManager.cancel(pendingIntentAlarm);
+		Log.d(TAG, "Alarm canceled:" +widgetId);
+	}
+
 }
